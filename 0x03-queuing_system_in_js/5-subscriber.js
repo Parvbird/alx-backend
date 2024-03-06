e5f7608c2f6da292e0b1439ff7fc686a077befa6
Joshua Kendu <kenduajayi@gmail.com>
2024-03-06 17:15:23 +0000

    modified file

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -1,26 +1,25 @@
 import { createClient } from 'redis';
 
-const publisher = createClient();
+const redisClient = createClient();
 
 //connect to redis server
-publisher.on('connect', function () {
-    console.log('Redis client connected to the server');
+redisClient.on('connect', function () {
+    console.log('Redis client connected to server');
 });
 
-publisher.on('error', function(error) {
-    console.log(`Redis client not connected to the server: ${error.message}`);
+redisClient.on('error', function (error) {
+    console.log(`Redis client not connected to server: ${error.message}`);
 });
 
-//function to publish message to holberton school channel
-function publishMessage(message, time) {
-  //message (str): message to be published, time (int): time in milliseconds to wait before sending message
-  setTimeout(function () {
-    console.log(`About to send ${message}`);
-    publisher.publish('holberton school channel', message);
-  }, time);
-}
+//subscribes holberton school channel
+redisClient.subscribe('holberton school channel');
 
-publishMessage("Holberton Student #1 starts course", 100);
-publishMessage("Holberton Student #2 starts course", 200);
-publishMessage("KILL_SERVER", 300);
-publishMessage("Holberton Student #3 starts course", 400);
+//listen for messages on channel print message when received
+redisClient.on('message', function (channel, message) {
+  console.log(`${message}`);
+  if (message === 'KILL_SERVER') {
+//unsubscribe from channel and cancel server connection
+    redisClient.unsubscribe('holberton school channel');
+    redisClient.end(true);
+  }
+});
